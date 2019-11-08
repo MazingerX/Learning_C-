e9883c4cc571d7543cfad9432e1fe28a01f2c2d4
mazingerx <lehinostroza@icloud.com>
2019-11-08 16:54:30 -0600

    Temperature converter from Celcius to Farenheight

---
+++ Temperature_Converter/Convert_fahrenheit_celsius/Convert_fahrenheit_celsius/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Convert_fahrenheit_celsius
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is the temperature in Celcius?");
+            double tempFaren = double.Parse(Console.ReadLine());
+            Console.WriteLine(TemperatureConverter(tempFaren));
+            double tempCelc = TemperatureConverter(tempFaren);
+            if (tempCelc >= 99)
+            {
+                Console.WriteLine("Dude you're sick!");
+            }
+
+        }
+
+        public static double TemperatureConverter(double var)
+        {
+            double temp = (var * (1.8) + 32);
+            return temp;
+        }
+    }
+}
